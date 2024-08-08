284338fde01669f4eee49acd93d0bdb8ec323da4
shengkai xia <karl@linek.at>
2024-08-09 05:52:48 +0800

    added testset for function get_group_elements

--- test/runtests.jl
+++ test/runtests.jl
@@ -20,4 +20,15 @@ using .Elements # Zugriff auf das Modul `Elements`
 
 end
 
-
+@testset "get group Elements" begin
+	elements = read_chemical_elements("../src/PeriodicTable.json")
+	group_elements = get_group_elements(elements::Vector{Element}, 1, false)
+	group_elements2 = get_group_elements(elements::Vector{Element}, "actinide", false)
+	group_elements3 = get_group_elements(elements::Vector{Element}, 13, true)	
+	@test length(group_elements) == 7
+	@test group_elements[1].name_de == "Wasserstoff"
+	@test group_elements[2].name_de == "Lithium"
+	@test group_elements2[1].name_de == "Actinium"
+	@test group_elements3[1].name_de == "Bor"
+	@test length(group_elements3) == 5
+end
