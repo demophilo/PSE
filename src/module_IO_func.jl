6264335f93b926ce6b03bd8f8681136528945beb
shengkai xia <karl@linek.at>
2024-08-12 10:32:59 +0800

    added function input_player_name

--- src/module_IO_func.jl
+++ src/module_IO_func.jl
@@ -1,11 +1,17 @@
 module IO_func
 
-export input_element
+export input_element, input_player_name
 
 function input_element()
 	print("Gib den Namen eines Elements ein: ")
 	trial_element = readline()
 	return trial_element
+end
+
+function input_player_name()
+	println("Geben Sie ihren Namen ein:")
+	_name::String = readline()
+	return _name
 end
 
 end
