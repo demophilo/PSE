0a66085106e0874cccc124a33262cef28767b665
Karl Linek <karl@linek.at>
2024-09-09 23:03:00 +0200

    added function call_function_by_name

--- src/game_variants.jl
+++ src/game_variants.jl
@@ -4,7 +4,7 @@ using JSON3
 include("elements.jl")
 using .Elements
 
-export Variant, create_path, parse_json_to_variants, input_game_type, get_elements_to_guess
+export Variant, create_path, parse_json_to_variants, input_game_type, get_elements_to_guess, call_function_by_name
 
 
 mutable struct Variant
@@ -18,12 +18,7 @@ end
 function create_path(directories::Vector{String}, filename::String)::String
 	return joinpath(directories..., filename)
 end
-#=
-directories = ["src"]
-filename = "variants.json"
-variants_path = create_path(directories, filename)
-println(variants_path)
-=#
+
 function parse_json_to_variants(file_path::String)::Dict{String, Variant}
 	json_data = read(file_path, String)
 	parsed_data = JSON3.read(json_data, Dict{String, Any})
@@ -52,16 +47,9 @@ function input_game_type(dict_game_variants::Dict{String, Variant})
 	return _chosen_game_letter
 end
 
-function get_elements_to_guess(dict_game_variants, game_type_key::String)
-	_elements_to_guess = dict_game_variants[game_type_key].target_elements
-	#if length(game_type_key) == 2
-	#    _elements_to_guess = get_elements_without_synthetic_elements(_elements_to_guess)
-	#end
-	return _elements_to_guess
+function call_function_by_name(mod::Module, func_name::String, params::AbstractVector)
+    func = getfield(mod, Symbol(func_name))
+    return func(params...)
 end
-#=
-for (key, value) in game_type
-	println(key, " ", value)
-end
-=#
+
 end # module
