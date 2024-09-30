67614a9194427099fecdffe3ff236b1e0896d814
Karl Linek <karl@linek.at>
2024-09-30 07:00:47 +0200

    emptied module

--- src/module_IO_func.jl
+++ src/module_IO_func.jl
@@ -1,17 +1,7 @@
 module IO_func
 
-export input_element, input_player_name
 
-function input_element()::String
-	print("Gib den Namen eines Elements ein: ")
-	trial_element = readline()
-	return trial_element
-end
 
-function input_player_name()
-	println("Geben Sie ihren Namen ein:")
-	_name::String = readline()
-	return _name
-end
+
 
 end
