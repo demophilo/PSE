878c741949567e69a38e60e39f9e15bc1372fb04
Karl Linek <karl@linek.at>
2024-10-02 21:36:11 +0200

    moved score calculation into gaming loop

--- src/chem.jl
+++ src/chem.jl
@@ -4,11 +4,6 @@ using JSON3
 include("../src/elements.jl")
 using .Elements
 
-
-
-
-
-
 # sets up the game
 clear_sreen()
 print_title()
@@ -16,21 +11,28 @@ println("\n"^15)
 # player section
 
 player_name = input_player_name()
-funfact_vector = ["Das Neutron wird von vielen Leuten als Element mit der Ordnungszahl 0 angesehen und Neutronium genannt.", "Das Neutronium geht keine chemsichen Bindungen ein und ist ergo ein Edelgas.", "Die Tochter von Marie Curie, Irene, machte das Experiment zusammen mit ihrem Ehemann, aber interpretierte die Experimente falsch. Sonst hätte sie das Neutron entdeckt.", "Das einzige Atom ohne Neutronen ist das sogenannte Protium - Wasserstoff-1.", "Obwohl es keine Verbindung mit Neutronen gibt, bestehen Neutronensterne nur aus Neutronen.", "Neutronen sabilisieren die Atomkerne. Die einzigen stablien Atomkerne mit mehr Protonen als Neutronen sind H-1 und He-3."]
-element_vector::Vector{Element} = read_json_to_element_vector("PeriodicTable.json")
-game_variant_vector::Vector{Variant} = read_json_to_variant_vector("variants.json")
-
+funfact_neutron_vector = ["Das Neutron wird von vielen Leuten als Element mit der Ordnungszahl 0 angesehen und Neutronium genannt.", "Das Neutronium geht keine chemischen Bindungen ein und ist ergo ein Edelgas.", "Die Tochter von Marie Curie, Irene, machte das Experiment zusammen mit ihrem Ehemann, aber interpretierte die Experimente falsch. Sonst hätte sie das Neutron entdeckt.", "Obwohl es keine Verbindung mit Neutronen gibt, bestehen Neutronensterne nur aus Neutronen.", "Neutronen sabilisieren die Atomkerne. Die einzigen stablien Atomkerne mit mehr Protonen als Neutronen sind H-1 und He-3.", "Neutronium ist ein Reinelement"]
+element_vector = read_json_to_element_vector("PeriodicTable.json")::Vector{Element}
+game_variant_vector = read_json_to_variant_vector("variants.json")::Vector{Variant}
+funfact::String = ""
+is_wide = false
 is_playing = true
 while is_playing
-	game_type::Variant = input_game_type(game_variant_vector)
+	local game_type = input_game_type(game_variant_vector)::Variant
+	
+	#####################################################
+	# gets the elements Data
+	#####################################################
+
 	element_to_guess_vector::Vector{Element} = get_elements_to_guess(element_vector::Vector{Element}, game_type.letter::String)
 	element_symbol_to_guess_vector::Vector{String} = [element.symbol for element in element_to_guess_vector]
 	element_name_to_guess_vector::Vector{String} = [element.name_de for element in element_to_guess_vector]
 	element_not_to_guess_vector::Vector{Element} = setdiff(element_vector, element_to_guess_vector)
 	element_symbols_not_to_guess_vector::Vector{String} = [element.symbol for element in element_not_to_guess_vector]
-	# sets up the initial values 
-	is_wide = false
 	periodic_table_matrix::Matrix{String} = get_PSE_matrix(element_vector, is_wide)
+	
+	# sets up the initial values 
+	
 	right_element_set::Set{Element} = Set{Element}()  # Initialisiert als Set von Elementen
 	right_element_symbols::Vector{String} = []
 	start_time = time()
@@ -40,7 +42,8 @@ while is_playing
 
 	# displays the first screen
 	show_matrix::Matrix{String} = get_PSE_ready_to_print(periodic_table_matrix, right_element_symbols, element_symbol_to_guess_vector, element_symbols_not_to_guess_vector)
-	display_screen(show_matrix, score, time_bonus)
+	global funfact = rand(funfact_neutron_vector)
+	display_screen(show_matrix, score, time_bonus, funfact)
 
 	# gaming loop
 	while length(element_to_guess_vector) > length(right_element_set)
@@ -52,7 +55,7 @@ while is_playing
 		end
 
 		if trial == "w" # to toggle from narrow to wide PSE
-			is_wide = !is_wide
+			global is_wide = !is_wide
 			periodic_table_matrix = get_PSE_matrix(is_wide)
 		end
 
@@ -69,21 +72,17 @@ while is_playing
 		end
 		clear_sreen()
 		show_matrix = get_PSE_ready_to_print(periodic_table_matrix, right_element_symbols, element_symbol_to_guess_vector, element_symbols_not_to_guess_vector)
-		display_screen(show_matrix, score, time_bonus)
+		end_time = time()
+		duration = end_time - start_time
+		score::Int = length(right_element_symbols) * 100
+		time_bonus::Int = (max_time - round(duration)) > 0 ? (max_time - round(duration)) * 10 : 0
+		global funfact = rand(chosen_element.funfact)
+		display_screen(show_matrix, score, time_bonus, funfact)
 	end # end of gaming loop
 
-	end_time = time()
-	duration = end_time - start_time
-	score::Int = length(right_element_symbols) * 100
-	time_bonus::Int = (max_time - round(duration)) > 0 ? (max_time - round(duration)) * 10 : 0
-
 	show_matrix = get_PSE_ready_to_print(periodic_table_matrix, right_element_symbols, element_symbol_to_guess_vector, element_symbols_not_to_guess_vector)
-	display_screen(show_matrix, score, time_bonus)
-
-
-
+	display_screen(show_matrix, score, time_bonus, funfact)
 	player::Player = Player(player_name, "PSE", game_type.name, "hard", score + time_bonus)
-
 	every_player_history_vector = read_players("chem_players_history.json")
 	actual_player_history_vector = [person for person in every_player_history_vector if person.name == player.name]
 	add_player_and_cut_top_n!(actual_player_history_vector, player, 3)
